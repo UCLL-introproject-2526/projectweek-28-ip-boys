0958f8849b274c3de08a599454905349007271da
Seppe De Greeve <seppe.degreeve@student.ucll.be>
2025-12-15 15:10:46 +0100

    screen in thMail.py

--- TherealGame/TheMain.py
+++ TherealGame/TheMain.py
@@ -11,3 +11,23 @@ def create_main_surface():
     pygame.display.set_mode(screen_size)
 
 create_main_surface()
+
+def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Create main surface
+    screen = create_main_surface()
+
+    # Infinite loop (later wordt dit je game loop)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+
+# Call main manually
+main()
